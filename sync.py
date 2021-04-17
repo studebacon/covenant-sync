5b2a5e79acdd4971abeaee2375fbf33e2da1a8dd
studebacon <64607989+studebacon@users.noreply.github.com>
2021-04-17 09:13:41 -0500

    Code cleanup and improvements

--- sync.py
+++ sync.py
@@ -13,60 +13,104 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 COVENANT_USERNAME = os.environ["COVENANT_USERNAME"]
 COVENANT_PASSWORD = os.environ["COVENANT_PASSWORD"]
 COVENANT_URL = os.environ["COVENANT_URL"]
-
 GHOSTWRITER_API_KEY = os.environ["GHOSTWRITER_API_KEY"]
 GHOSTWRITER_URL = os.environ["GHOSTWRITER_URL"]
 GHOSTWRITER_OPLOG_ID = os.environ["GHOSTWRITER_OPLOG_ID"]
 REDIS_HOSTNAME =os.environ["REDIS_HOSTNAME"]
 
-gwhead = {'Authorization': f"Api-Key {GHOSTWRITER_API_KEY}", "Content-Type": "application/json"}
-tj={}
-while True:
-    authheader = {"accept":"text/plain","Content-Type":"application/json-patch+json"}
-    authdata = {"userName":f"{COVENANT_USERNAME}","password":f"{COVENANT_PASSWORD}"}
+SLEEP_TIME = 30
+
+rconn = redis.Redis(host=f"{REDIS_HOSTNAME}", port=6389, db=0)
+gw_header = {'Authorization': f"Api-Key {GHOSTWRITER_API_KEY}", "Content-Type": "application/json"}
+cov_authheader = {"accept":"text/plain","Content-Type":"application/json-patch+json"}
+cov_authdata = {"userName":f"{COVENANT_USERNAME}","password":f"{COVENANT_PASSWORD}"}
+cmds={}
+
+def getCovToken():
+    cov_token = {}
     try:
-        r = requests.post(f"{COVENANT_URL}/api/users/login", data=json.dumps(authdata), 
-                headers=authheader, verify=False)
-        rj = json.loads(r.text)
+        cov_authresp = json.loads(requests.post(f"{COVENANT_URL}/api/users/login", 
+            data=json.dumps(cov_authdata), headers=cov_authheader, verify=False).text)
+        cov_token = {"Authorization":"Bearer " + cov_authresp["covenantToken"]}
+    except Exception as e:
+        print(e)
+    return cov_token
+
+def newOpFromCmd(cmd):
+    op_data = {}
+    op_data["oplog_id"] = GHOSTWRITER_OPLOG_ID
+
+    if cmd["gruntTasking"]["taskingTime"] != "0001-01-01T00:00:00":
+        start = datetime.strptime(cmd["gruntTasking"]["taskingTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+        op_data["start_date"] = start.strftime("%Y-%m-%d %H:%M:%S")
+    if cmd["gruntTasking"]["completionTime"] != "0001-01-01T00:00:00":
+        end = datetime.strptime(cmd["gruntTasking"]["completionTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+        op_data["end_date"] = end.strftime("%Y-%m-%d %H:%M:%S")
+
+    op_data["description"] = cmd["gruntTasking"]["gruntTask"]["description"] if cmd["gruntTasking"]["gruntTask"]["description"] is not None else ""
+    op_data["tool"] = "Covenant - " + cmd["gruntTasking"]["gruntTask"]["name"] if cmd["gruntTasking"]["gruntTask"]["name"] is not None else ""
+    op_data["command"] = cmd["command"] if cmd["command"] is not None else ""
+    op_data["operator_name"] = cmd["user"]["userName"] if cmd["user"]["userName"] is not None else ""
+
+    try:
+        auth_token = getCovToken()
+        gruntid = cmd["gruntId"] if cmd["gruntId"] is not None else ""
+        grunt = json.loads(requests.get(f"{COVENANT_URL}/api/grunts/{gruntid}", headers=auth_token, verify=False).text)
+
+        if grunt:
+            op_data["user_context"] = grunt["userName"] if grunt["userName"] is not None else ""
+            op_data["source_ip"] = grunt["hostname"] + " - " + grunt["ipAddress"]
+
+        cmdid = str(cmd["commandOutputId"]) if cmd["commandOutputId"] is not None else ""
+        cmdout = json.loads(requests.get(f"{COVENANT_URL}/api/commandoutputs/{cmdid}", headers=auth_token, verify=False).text)
+        if cmdout:
+            op_data["output"] = cmdout["output"] if cmdout["output"] is not None else ""
+    except Exception as e:
+        print(e)                        
+
+    return op_data
+
+def updateOpFromCmd(cmd):
+    op_data = {}
     
-        tokenheader = {"Authorization":"Bearer " + rj["covenantToken"]}
-        taskings = requests.get(f"{COVENANT_URL}/api/commands", headers=tokenheader, verify=False)
-        tj = json.loads(taskings.text)
+    try:
+        auth_token = getCovToken()
+        cmdid = str(cmd["commandOutputId"]) if cmd["commandOutputId"] is not None else ""
+        cmdout = json.loads(requests.get(f"{COVENANT_URL}/api/commandoutputs/{cmdid}", headers=auth_token, verify=False).text)
+        if cmdout:
+            if cmdout["output"] != "": 
+                if cmd["gruntTasking"]["taskingTime"] != "0001-01-01T00:00:00":
+                    start = datetime.strptime(cmd["gruntTasking"]["taskingTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+                    if cmd["gruntTasking"]["completionTime"] != "0001-01-01T00:00:00":
+                        end = datetime.strptime(cmd["gruntTasking"]["completionTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+                        #only set values if we have all of them
+                        op_data["output"] = cmdout["output"]
+                        op_data["end_date"] = end.strftime("%Y-%m-%d %H:%M:%S")
+                        op_data["start_date"] = start.strftime("%Y-%m-%d %H:%M:%S")
+    except Exception as e:
+        print(e)
+    return op_data
+
+
+#main loop
+while True:
+    try:
+        auth_token = getCovToken()
+        cmds = json.loads(requests.get(f"{COVENANT_URL}/api/commands", headers=auth_token, verify=False).text)
     except Exception as e:
         print(e)
 
-    if tj is not None:
-        for rec in tj:
-            if rec["gruntTasking"]:
-                commandid = rec["id"]
-                rconn = redis.Redis(host=f"{REDIS_HOSTNAME}", port=6379, db=0)
+    if cmds is not None:
+        for cmd in cmds:
+            if cmd["gruntTasking"]:
+                commandid = cmd["id"] if cmd["id"] is not None else ""
                 entry = rconn.get(str(commandid))
-                if entry is None:
-                    gw_message = {}
-                    gw_message["oplog_id"] = GHOSTWRITER_OPLOG_ID
-                    gruntid = rec["gruntId"]
-                    if rec["gruntTasking"]["taskingTime"] != "0001-01-01T00:00:00":
-                        start = datetime.strptime(rec["gruntTasking"]["taskingTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
-                        gw_message["start_date"] = start.strftime("%Y-%m-%d %H:%M:%S")
-                    if rec["gruntTasking"]["completionTime"] != "0001-01-01T00:00:00":
-                        end = datetime.strptime(rec["gruntTasking"]["completionTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
-                        gw_message["end_date"] = end.strftime("%Y-%m-%d %H:%M:%S")
-                    gw_message["description"] = rec["gruntTasking"]["gruntTask"]["description"] if rec["gruntTasking"]["gruntTask"]["description"] is not None else ""
-                    gw_message["tool"] = "Covenant - " + rec["gruntTasking"]["gruntTask"]["name"] if rec["gruntTasking"]["gruntTask"]["name"] is not None else ""
-                    gw_message["command"] = rec["command"] if rec["command"] is not None else ""
-                    gw_message["operator_name"] = rec["user"]["userName"] if rec["user"]["userName"] is not None else ""
+                if not entry:
+                    #create new oplog entry
                     try:
-                        grunt = json.loads(requests.get(f"{COVENANT_URL}/api/grunts/"+str(gruntid), 
-                            headers=tokenheader, verify=False).text)
-                        if grunt is not None:
-                            gw_message["user_context"] = grunt["userName"] if grunt["userName"] is not None else ""
-                            gw_message["source_ip"] = grunt["hostname"] + " - " + grunt["ipAddress"]
-                        cmdout = json.loads(requests.get(f"{COVENANT_URL}/api/commandoutputs/"+str(rec["commandOutputId"]), 
-                            headers=tokenheader, verify=False).text)
-                        if cmdout is not None:
-                            gw_message["output"] = cmdout["output"] if cmdout["output"] is not None else ""
+                        gw_message = newOpFromCmd(cmd) 
                         gwr = requests.post(f"{GHOSTWRITER_URL}/oplog/api/entries/", 
-                                data=json.dumps(gw_message), headers=gwhead, verify=False)
+                                data=json.dumps(gw_message), headers=gw_header, verify=False)
                         if gwr.status_code != 201:
                             print("Error posting to ghostwriter " + str(gwr.status_code))
                         else:
@@ -74,29 +118,26 @@ while True:
                     except Exception as e:
                         print(e)                        
                 else:
+                    #update existing oplog
                     gw_message = {}
                     redisdata = json.loads(entry.decode())
-                    if redisdata is not None:
+                    if redisdata:
+                        #only update if previous output value was empty
                         if redisdata["output"] == "" and redisdata["id"] != "":
-                            entry_id = str(redisdata["id"])
-                            try:
-                                cmdout = json.loads(requests.get(f"{COVENANT_URL}/api/commandoutputs/"+str(rec["commandOutputId"]), 
-                                    headers=tokenheader, verify=False).text)
-                                if cmdout != None:
-                                    gw_message["output"] = redisdata["output"] = cmdout["output"] if cmdout["output"] is not None else ""
-                                    if rec["gruntTasking"]["taskingTime"] != "0001-01-01T00:00:00":
-                                        start = datetime.strptime(rec["gruntTasking"]["taskingTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
-                                        gw_message["start_date"] = redisdata["start_date"] = start.strftime("%Y-%m-%d %H:%M:%S")
-                                    if rec["gruntTasking"]["completionTime"] != "0001-01-01T00:00:00":
-                                        end = datetime.strptime(rec["gruntTasking"]["completionTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
-                                        gw_message["end_date"] = redisdata["end_date"] = end.strftime("%Y-%m-%d %H:%M:%S")
-                                
-                                    gwr = requests.put(f"{GHOSTWRITER_URL}/oplog/api/entries/"+ entry_id + "/?format=json", 
-                                            data=json.dumps(gw_message), headers=gwhead, verify=False)
+                            op_id = str(redisdata["id"])
+                            gw_message = updateOpFromCmd(cmd)
+                            if gw_message:
+                                try:
+                                    gwr = requests.put(f"{GHOSTWRITER_URL}/oplog/api/entries/{op_id}/?format=json", 
+                                            data=json.dumps(gw_message), headers=gw_header, verify=False)
                                     if gwr.status_code != 200:
                                         print("Error posting to ghostwriter " + str(gwr.status_code))
                                     else:
+                                        #update the date and output values in redis
+                                        redisdata["output"] = gw_message["output"]
+                                        redisdata["start_date"] = gw_message["start_date"]
+                                        redisdata["end_date"] = gw_message["end_date"]
                                         rconn.set(str(commandid),json.dumps(redisdata))
-                            except Exception as e:
-                                print(e)
-    time.sleep(30)
+                                except Exception as e:
+                                    print(e)
+    time.sleep(SLEEP_TIME)
